e48cf8ba452dc6b9a6dcc8a2d194b7db63a250b6
John Thomas <thomas07@vt.edu>
2017-04-24 07:40:48 -0700

    Adding some benchmark tests before I refactor
    - Since I have a working solution, I want to set up some baseline
      benchmark tests to compare any new solutions I come to during
      refactoring. There are pretty much 2 things we care about here:
      1. Memory allocation
      2. CPU cycles
    
    - Test results for first solution:
    
    ```
    $ bundle exec ruby bench/memory.rb
    Calculating -------------------------------------
                    zero    80.000  memsize (     0.000  retained)
                             2.000  objects (     0.000  retained)
                             1.000  strings (     0.000  retained)
            single_digit   728.000  memsize (     0.000  retained)
                            15.000  objects (     0.000  retained)
                             4.000  strings (     0.000  retained)
                    tens   648.000  memsize (     0.000  retained)
                            13.000  objects (     0.000  retained)
                             3.000  strings (     0.000  retained)
           below_hundred     1.336k memsize (     0.000  retained)
                            27.000  objects (     0.000  retained)
                             5.000  strings (     0.000  retained)
               thousands     4.448k memsize (     0.000  retained)
                            80.000  objects (     0.000  retained)
                             8.000  strings (     0.000  retained)
                millions     8.192k memsize (     0.000  retained)
                           144.000  objects (     0.000  retained)
                            11.000  strings (     0.000  retained)
    
    Comparison:
                    zero:         80 allocated
                    tens:        648 allocated - 8.10x more
            single_digit:        728 allocated - 9.10x more
           below_hundred:       1336 allocated - 16.70x more
               thousands:       4448 allocated - 55.60x more
                millions:       8192 allocated - 102.40x more
    ```
    
    ```
    $ bundle exec ruby bench/ips.rb
    Warming up --------------------------------------
                    zero   136.280k i/100ms
            single_digit    33.043k i/100ms
                    tens    36.316k i/100ms
           below_hundred    18.566k i/100ms
               thousands     5.397k i/100ms
                millions     2.806k i/100ms
    Calculating -------------------------------------
                    zero      2.247M (± 1.0%) i/s -     11.311M in   5.033738s
            single_digit    374.405k (± 0.9%) i/s -      1.883M in   5.030899s
                    tens    414.591k (± 0.9%) i/s -      2.106M in   5.080869s
           below_hundred    197.458k (± 1.2%) i/s -      1.003M in   5.078173s
               thousands     55.320k (± 0.9%) i/s -    280.644k in   5.073505s
                millions     28.961k (± 0.8%) i/s -    145.912k in   5.038435s
    
    Comparison:
                    zero:  2247295.4 i/s
                    tens:   414590.8 i/s - 5.42x  slower
            single_digit:   374404.6 i/s - 6.00x  slower
           below_hundred:   197457.7 i/s - 11.38x  slower
               thousands:    55320.1 i/s - 40.62x  slower
                millions:    28961.4 i/s - 77.60x  slower
    ```

---
+++ bench/ips.rb
@@ -0,0 +1,42 @@
+require 'benchmark/ips'
+require 'number_to_word'
+
+def zero
+  NumberToWord.new(0).word
+end
+
+def single_digit
+  NumberToWord.new(7).word
+end
+
+def tens
+  NumberToWord.new(18).word
+end
+
+def below_hundred
+  NumberToWord.new(84).word
+end
+
+def thousands
+  NumberToWord.new(4321).word
+end
+
+def millions
+  NumberToWord.new(1_856_231).word
+end
+
+
+Benchmark.ips do |x|
+  x.config(:time => 5, :warmup => 2)
+
+  x.report("zero")          { zero }
+  x.report("single_digit")  { single_digit }
+  x.report("tens")          { tens }
+  x.report("below_hundred") { below_hundred }
+  x.report("thousands")     { thousands }
+  x.report("millions")      { millions }
+
+  # Compare the iterations per second of the various reports!
+  x.compare!
+end
+
---
+++ bench/memory.rb
@@ -0,0 +1,38 @@
+require "benchmark/memory"
+require 'number_to_word'
+
+def zero
+  NumberToWord.new(0).word
+end
+
+def single_digit
+  NumberToWord.new(7).word
+end
+
+def tens
+  NumberToWord.new(18).word
+end
+
+def below_hundred
+  NumberToWord.new(84).word
+end
+
+def thousands
+  NumberToWord.new(4321).word
+end
+
+def millions
+  NumberToWord.new(1_856_231).word
+end
+
+Benchmark.memory do |x|
+  x.report("zero")          { zero }
+  x.report("single_digit")  { single_digit }
+  x.report("tens")          { tens }
+  x.report("below_hundred") { below_hundred }
+  x.report("thousands")     { thousands }
+  x.report("millions")      { millions }
+
+  x.compare!
+end
+
